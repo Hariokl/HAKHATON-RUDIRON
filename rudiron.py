3b44307a435a37e8efd4202a59716d41be634672
Ipopuk <honenene@yandex.ru>
2024-11-23 17:11:00 +0300

    upload code to arduino

--- rudiron.py
+++ rudiron.py
@@ -1,11 +1,38 @@
-import serial
-import time
+import subprocess
 
-ArduinoSerial = serial.Serial('COM3', 9600)
-time.sleep(2)
+sketch_path = 'C:\\Users\\PC\\Documents\\Arduino\\sketch_nov23a\\sketch_nov23a.ino'  # Use double backslashes in Windows paths
 
-# print(ArduinoSerial.readline())
-print("Enter 1 to turn ON LED and 0 to turn OFF LED")
+fqbn = 'Rudiron:MDR32F9Qx:buterbrodR916'
 
-while True:
-    ArduinoSerial.write(b'1')
+
+port = 'COM3'
+
+print("Compiling the sketch...")
+compile_command = ['arduino-cli', 'compile', '--fqbn', fqbn, sketch_path]
+compile_process = subprocess.run(
+    compile_command, capture_output=True, text=True, encoding='utf-8'
+)
+
+if compile_process.returncode != 0:
+    print("Compilation failed:")
+    print(compile_process.stderr)
+    exit(1)
+else:
+    print("Compilation successful.")
+
+print("Uploading the sketch...")
+upload_command = ['arduino-cli', 'upload', '-p', port, '--fqbn', fqbn, sketch_path]
+upload_process = subprocess.run(
+    upload_command, capture_output=True, text=True, encoding='utf-8'
+)
+
+if upload_process.returncode != 0:
+    print("Upload failed:")
+    print(upload_process.stderr)
+    exit(1)
+else:
+    print("Upload successful.")
+
+
+# arduino-cli compile --fqbn Rudiron:MDR32F9Qx:buterbrodR916 "C:\\Users\\PC\\Documents\\Arduino\\sketch_nov23a\\sketch_nov23a.ino"
+# arduino-cli upload -p COM3 --fqbn Rudiron:MDR32F9Qx:buterbrodR916 --verbose "C:\Users\PC\Documents\Arduino\sketch_nov23a"
